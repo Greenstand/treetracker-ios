bf3c448bef719798c64233f694e02043a128c8dd
Alex Cornforth <alexcornforth@me.com>
2020-05-15 11:03:12 +0100

    #4: Added localizable strings

---
+++ TreeTracker/SwiftGen/Strings.swift
@@ -0,0 +1,44 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name
+internal enum L10n {
+
+  internal enum App {
+    /// Greenstand Tree Tracker
+    internal static let title = L10n.tr("Localizable", "App.Title")
+  }
+
+  internal enum SignIn {
+    internal enum Email {
+      /// Or Email Address
+      internal static let placeholder = L10n.tr("Localizable", "SignIn.Email.Placeholder")
+    }
+    internal enum PhoneNumber {
+      /// Phone Number
+      internal static let placeholder = L10n.tr("Localizable", "SignIn.PhoneNumber.Placeholder")
+    }
+  }
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    // swiftlint:disable:next nslocalizedstring_key
+    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+private final class BundleToken {}
