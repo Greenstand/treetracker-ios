69410ef35e3fed3330379147c0efa63c3da4a912
Alex Cornforth <alexcornforth@me.com>
2020-06-29 13:03:44 +0100

    #4: Added validation to name model

--- TreeTracker/Models/Name.swift
+++ TreeTracker/Models/Name.swift
@@ -12,3 +12,45 @@ struct Name {
     let firstName: String
     let lastName: String
 }
+
+// MARK: - Validation
+extension Name {
+
+    enum NameValidationError: Error {
+        case firstNameInvalid
+        case lastNameInvalid
+        case missingFirstName
+        case missingLastName
+        case empty
+    }
+
+    enum NameValidationResult {
+        case valid
+        case invalid(NameValidationError)
+    }
+
+    var isValid: NameValidationResult {
+        switch (firstNameValid, lastNameValid) {
+        case (.valid, .valid):
+            return .valid
+        case (.invalid, _):
+            return .invalid(.firstNameInvalid)
+        case (_, .invalid):
+            return .invalid(.lastNameInvalid)
+        case (.empty, .empty):
+            return .invalid(.empty)
+        case (.empty, _):
+            return .invalid(.missingFirstName)
+        case (_, .empty):
+            return .invalid(.missingLastName)
+        }
+    }
+
+    var firstNameValid: Validation.Result {
+        return Validation.validate(firstName, type: .firstName)
+    }
+
+    var lastNameValid: Validation.Result {
+        return Validation.validate(lastName, type: .lastName)
+    }
+}
