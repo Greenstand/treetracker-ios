b5ec365e1eb5f8bdf9375169921aee2061c5effd
Remi <hai.remi@gmail.com>
2020-08-11 12:44:23 -0400

    added collectionview

---
+++ TreeTracker/UI/Views/Pictures/PicturesCollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PicturesCollectionViewController.swift
+//  TreeTracker
+//
+//  Created by Remi Varghese on 8/10/20.
+//  Copyright © 2020 Greenstand. All rights reserved.
+//
+
+import UIKit
+
+class PicturesCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
