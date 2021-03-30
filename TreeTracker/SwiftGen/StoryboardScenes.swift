a43baa713ed84211d57eaf8fae51378c6a7718b6
Remi <remi.ios333@gmail.com>
2021-03-30 11:39:53 -0400

    added front end ui Profile Screen

--- TreeTracker/SwiftGen/StoryboardScenes.swift
+++ TreeTracker/SwiftGen/StoryboardScenes.swift
@@ -32,6 +32,9 @@ internal enum StoryboardScene {
 
     internal static let initialScene = InitialSceneType<TreeTracker.LoadingViewController>(storyboard: Loading.self)
   }
+  internal enum Profile: StoryboardType {
+    internal static let storyboardName = "Profile"
+  }
   internal enum Selfie: StoryboardType {
     internal static let storyboardName = "Selfie"
 
