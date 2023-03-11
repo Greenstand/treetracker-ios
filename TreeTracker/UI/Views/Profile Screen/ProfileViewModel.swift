fc2c967304e7c7281aeba9721c920475df65bb36
Frédéric Helfer <fre.helfer@gmail.com>
2023-03-11 13:00:36 -0300

    fix: photo quality update to the default value when the user deletes the account.

--- TreeTracker/UI/Views/Profile Screen/ProfileViewModel.swift
+++ TreeTracker/UI/Views/Profile Screen/ProfileViewModel.swift
@@ -28,17 +28,20 @@ class ProfileViewModel {
     private let selfieService: Treetracker_Core.SelfieService
     private let uploadManager: Treetracker_Core.UploadManaging
     private let userDeletionService: Treetracker_Core.UserDeletionService
+    private let settingsService: Treetracker_Core.SettingsService
 
     init(
         planter: Treetracker_Core.Planter,
         selfieService: Treetracker_Core.SelfieService,
         uploadManager: Treetracker_Core.UploadManaging,
-        userDeletionService: Treetracker_Core.UserDeletionService
+        userDeletionService: Treetracker_Core.UserDeletionService,
+        settingsService: Treetracker_Core.SettingsService
     ) {
         self.planter = planter
         self.selfieService = selfieService
         self.uploadManager = uploadManager
         self.userDeletionService = userDeletionService
+        self.settingsService = settingsService
     }
 
     var title: String {
@@ -80,6 +83,7 @@ class ProfileViewModel {
     func deleteAccount() {
 
         do {
+            settingsService.update(value: PhotoQuality.medium.photoSize, forSetting: .treePhotoSize)
             try userDeletionService.deletePlanter(planter: planter)
             if uploadManager.isUploading {
                 uploadManager.stopUploading()
